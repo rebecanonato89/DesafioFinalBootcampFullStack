5f94d3153249a40049c7632eb88a410e19a40955
Rebeca <rebeca.nonato.lemaf@gmail.com>
2020-07-22 20:48:33 -0300

    update e delete back

--- backend/src/controllers/TransactionController.js
+++ backend/src/controllers/TransactionController.js
@@ -1,6 +1,11 @@
 const Transaction = require('../models/TransactionModel');
 const { response } = require('express');
 //index, show, store, update, destroy
+
+function formataDate(yearMonthDay){
+    return yearMonthDay.split('-');
+}
+
 module.exports = {
     async index (req, res) {
         const transaction = await Transaction.find();
@@ -17,11 +22,7 @@ module.exports = {
             type 
         } = req.body;
 
-        const date = yearMonthDay.split('-');
-        const year = date[0];
-        const month = date[1];
-        const day = date[2];
-        const yearMonth = year+'-'+month;
+        const date = formataDate(yearMonthDay);
 
         let transaction = await Transaction.findOne({ description, value, category, yearMonthDay });
         if(!transaction){
@@ -29,10 +30,10 @@ module.exports = {
                 description, 
                 value, 
                 category, 
-                year, 
-                month, 
-                day,
-                yearMonth, 
+                year: date[0], 
+                month: date[1], 
+                day: date[2],
+                yearMonth: date[0]+'-'+date[1], 
                 yearMonthDay, 
                 type });
         }
@@ -40,25 +41,60 @@ module.exports = {
     },
 
     async update(req, res){
-        const id = req.params;
-        if(id){
-            let transaction = await Transaction.findById({ id });
-        };
-        if(transaction){
+        const { id } = req.params;
+        const { 
+            description, 
+            value, 
+            category, 
+            yearMonthDay, 
+            type 
+        } = req.body;
 
+        const date = formataDate(yearMonthDay);
+        try {
+            if(id){
+                let transaction = await Transaction.findById(id);
+                if(transaction){
+                    await Transaction.findOneAndUpdate(
+                        id,
+                        { description, 
+                            value, 
+                            category, 
+                            year: date[0], 
+                            month: date[1], 
+                            day: date[2],
+                            yearMonth: date[0]+'-'+date[1], 
+                            yearMonthDay, 
+                            type,
+                        },
+                        {useFindAndModify: false}
+                    );
+                    console.log(transaction);
+                    return res.json({ transaction });
+                }
+                return res.json({ message: "Registro não existe!" });
+            };
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
         }
     },
 
     async delete(req, res){
         const { id } = req.params;
 
-        const transaction = await Transaction.findById(id);
+        try{
+            const transaction = await Transaction.findById(id);
 
-        console.log(transaction);
-        if(transaction){
-            await Transaction.findById(id).deleteOne();
+            console.log(transaction);
+            if(transaction){
+                await Transaction.findById(id).deleteOne();
+            }
+            return res.status(204).send();
+        } catch (error) {
+            res.status(500).send(error);
         }
-        return res.status(204).send();
     }
 
 };
